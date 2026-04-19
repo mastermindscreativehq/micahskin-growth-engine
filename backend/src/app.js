02d48c0f71093096a05b9ae76e11da61935c7ab3
recoveryservicex <masteringaitoday@gmail.com>
2026-04-19 12:13:04 +0100

    fix: correct CORS config

--- backend/src/app.js
+++ backend/src/app.js
@@ -49,18 +49,11 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.options('*', cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-
-    return callback(new Error('Not allowed by CORS'));
-  },
+  origin: allowedOrigins,
   credentials: true,
 }));
 
